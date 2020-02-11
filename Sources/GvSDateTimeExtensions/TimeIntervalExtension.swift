e820bce9cc44b7896b1740dde36aac0e1a6b45b7
Grant van Staden <grant.vanstaden@live.com.au>
2020-02-12 00:02:30 +1000

    Removed unnecessary public modifiers.

--- Sources/GvSDateTimeExtensions/TimeIntervalExtension.swift
+++ Sources/GvSDateTimeExtensions/TimeIntervalExtension.swift
@@ -9,28 +9,28 @@ import Foundation
 
 public extension TimeInterval {
     /// Extracts housr from this time interval
-    public var hours: Int {
+    var hours: Int {
         get {
            (Int(floor(self)) - (Int(floor(self)) % 3600)) / 3600
         }
     }
     
     /// Extracts the minutes from this time interval.
-   public var minutes: Int {
+   var minutes: Int {
         get {
             (Int(floor(self)) % 3600) / 60
         }
     }
     
     /// Extracts seconds from this time interval
-   public var seconds: Int {
+   var seconds: Int {
         get {
             (Int(floor(self)) % 3600) - (60 * ((Int(floor(self)) % 3600) / 60))
         }
     }
     
     /// The time format string representation of thie time interval (HH:MM:SS)
-    public var timeString: String {
+    var timeString: String {
         get {
             let hours = self.hours.asLeftPaddedString(ofLength: 2)
             let minutes = self.minutes.asLeftPaddedString(ofLength: 2)
